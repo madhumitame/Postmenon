077b04384b702f4677c214c12872e2f087a6f650
Madhumita Menon <madhumitamenon2000@gmail.com>
2026-04-13 16:14:33 +0100

    Update create-payment.js

--- netlify/functions/create-payment.js
+++ netlify/functions/create-payment.js
@@ -9,14 +9,12 @@ exports.handler = async function(event) {
   try {
     data = JSON.parse(event.body);
   } catch(e) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Invalid request" })
-    };
+    return { statusCode: 400, body: JSON.stringify({ error: "Invalid request" }) };
   }
 
   const to = data.to || {};
-  const baseUrl = event.headers.origin || "https://yourdomain.com";
+  const from = data.from || {};
+  const baseUrl = event.headers.origin || "https://postacard.netlify.app";
 
   try {
     const session = await stripe.checkout.sessions.create({
@@ -33,20 +31,30 @@ exports.handler = async function(event) {
         quantity: 1
       }],
       mode: "payment",
-
       metadata: {
-        postcard: JSON.stringify(data)
+        message:       (data.message || "").substring(0, 400),
+        to_name:       (to.name || "").substring(0, 100),
+        to_address1:   (to.address || "").substring(0, 100),
+        to_address2:   (to.address2 || "").substring(0, 100),
+        to_city:       (to.city || "").substring(0, 100),
+        to_state:      (to.state || "").substring(0, 100),
+        to_zip:        (to.zip || "").substring(0, 20),
+        to_country:    (to.country || "").substring(0, 2),
+        from_name:     (from.name || "").substring(0, 100),
+        from_address1: (from.address || "").substring(0, 100),
+        from_address2: (from.address2 || "").substring(0, 100),
+        from_city:     (from.city || "").substring(0, 100),
+        from_state:    (from.state || "").substring(0, 100),
+        from_zip:      (from.zip || "").substring(0, 20),
+        from_country:  (from.country || "").substring(0, 2)
       },
-      //success_url: baseUrl + "/success?session_id={CHECKOUT_SESSION_ID}",
-      success_url: baseUrl + "/?success=true", //to go to final page
-      cancel_url: baseUrl + "/?cancelled=true"
+      success_url: baseUrl + "/?success=true",
+      cancel_url:  baseUrl + "/?cancelled=true"
     });
 
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ url: session.url })
     };
 
